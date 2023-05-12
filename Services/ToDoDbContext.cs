6aaf82c50e64f35dca452ae5e18bc4cf8da93561
Your Name <ramoneete93@gmail.com>
2023-05-11 22:18:30 -0400

    Iniciando api

---
+++ Services/ToDoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace APIUSUARIOS
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options) { }
+
+        public DbSet<Entities.User> Users { get; set; }
+
+    }
+}
